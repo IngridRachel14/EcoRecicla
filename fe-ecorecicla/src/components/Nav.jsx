de1def9cc5d1f8e8e7dbd6183888dc92c5c9a89f
Luis2601 <chitinantonio2601@gmail.com>
2026-06-06 02:35:26 -0500

    Arreglando nav
    Error en zona educativa

--- fe-ecorecicla/src/components/Nav.jsx
+++ fe-ecorecicla/src/components/Nav.jsx
@@ -124,7 +124,7 @@ export default function Navbar() {
             {/* Navegación más cerca del logo */}
             <nav className="flex flex-col px-4 pt-2 space-y-2">
               <Link
-                href="/Referidos"
+                href="/ZonaEducativa"
                 className="bg-white hover:bg-gray-200 text-black px-4 py-2 rounded-lg transition duration-200 shadow-sm"
               >
                 Zona Educativa
